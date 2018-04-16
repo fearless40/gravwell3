ed3e488ff5717f0221a29d91a948838ed5d5dae3
Adam Spivack <spivack@outlook.com>
2018-04-15 23:09:28 -0400

    Continue to work on broad phase (really fine fase collision detection)

--- src/game-gwell/BroadCollisions.cpp
+++ src/game-gwell/BroadCollisions.cpp
@@ -1,6 +1,7 @@
 #include "../stdafx.h"
 #include "BroadCollisions.h"
 #include "Position.h"
+#include <algorithm>
 
 
 namespace BroadCollisions {
@@ -11,7 +12,7 @@ namespace BroadCollisions {
 		Game::Coord x1, y1, x2, y2;
 	};
 
-	//Sorted by ids
+	//Unsorted
 	Entity::Vector ids;
 	std::vector<partialabb> pabbs;
 	std::vector<uint32_t> teams;
@@ -19,20 +20,45 @@ namespace BroadCollisions {
 	std::vector<fullabb> cache;
 
 	bool overlaps(const fullabb & lhs, const fullabb & rhs) {
-		return  (lhs.x  < rhs.x2)	&&
-				(lhs.x2 > rhs.x )	&&
-				(lhs.y  < rhs.y2)	&&
-				(lhs.y2 > rhs.y );
+		return  (lhs.x1 < rhs.x2)	&&
+				(lhs.x2 > rhs.x1)	&&
+				(lhs.y1 < rhs.y2)	&&
+				(lhs.y2 > rhs.y1);
 	};
 
+	fullabb make_aabb(const Position::Pos & pos, const partialabb & abb) {
+		return {
+			pos.x, pos.y,
+			pos.x + abb.width,
+			pos.y + abb.height
+		};
+	}
+
+	void update_cache(const Entity::Vector & eId, const Position::Positions & pos) {
+		auto idsBegin = eId.begin();
+		auto idsEnd = eId.end();
+		auto posBegin = pos.begin();
+		for (const auto x : ids) {
+			auto item = std::lower_bound(idsBegin, idsEnd, x );
+			if (item != idsBegin && *item == x) {
+				auto srcDistance = std::distance(idsBegin, item);
+				auto dstDistance = &ids[0] - &x;
+				cache[dstDistance] = make_aabb(pos[srcDistance], pabbs[dstDistance]);
+			}
+		}
+	}
+
 	void update() {
-		auto changed{ Positions::getChanged().get() };
+		auto changed{ Position::getChanged().get() };
 		std::sort(changed.begin(), changed.end());
-		auto updatedPositions = Positions::get(Temp<decltype(changed), Sorted>{changed});
+		auto updatedPositions = Position::get({changed}).get();
+
+		//Regenerate cache
+		assert(changed.size() == updatedPositions.size()); //The two must be of the same size
+
+		update_cache(changed, updatedPositions);
 
 
 	}
 }
 
-1,1 4,4
-2,2 3,3
