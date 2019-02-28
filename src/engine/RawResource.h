e901ac4a9bf043ba8526b52b0d31a66bb5ba65ff
Adam Spivack <spivack@outlook.com>
2019-02-27 16:43:23 -0800

    Added raw file loader. That uses the file system.

--- src/engine/RawResource.h
+++ src/engine/RawResource.h
@@ -1,28 +1,48 @@
 #pragma once
 
+#include <optional>
 #include <filesystem>
 #include <variant>
 #include <memory>
+#include <gsl/span>
+#include <filesystem>
 
 namespace Engine::Resources {
+
+	using RawID_IntID = uint64_t;
+	using RawID_StringID = std::filesystem::path;
+
 	using RawID = std::variant<uint64_t, std::filesystem::path>;
 
 	struct RawResource {
-		std::unique_ptr<std::byte> data;
+		std::unique_ptr<std::byte> file_data;
 		std::size_t size;
 		RawID id;
+
+		gsl::span<std::byte> asSpan() {
+			return { file_data.get(), size };
+		}
+//		FileInfo * fileinfo;
 	};
 
+/*	struct FileInfo {
+		std::string file_ending;
+		std::size_t file_size;
+		bool		isWritable;
+	};*/
+
+/*	class FileInfo {
+	public:
+		virtual ~FileInfo() = 0;
+		virtual  FileInfo getFileInformation(RawID id);
+	};
+*/
 	class RawLoader {
 	public:
 		virtual ~RawLoader() = 0;
-		virtual RawResource read_item(RawID id) = 0;
-		virtual bool		has_item(RawID id) = 0;
+		virtual std::optional<RawResource> read_item(RawID id) const = 0;
+		virtual bool		has_item(RawID id) const = 0;
 	};
 
 
-	RawResource LoadFile(uint32_t raw_id);
-	RawResource LoadFile(std::string file_name);
-
-	void		SetLoader(std::unique_ptr<RawLoader> loader, int priority);
 }
---
+++ src/main/FileSystemRawLoader.cpp
@@ -0,0 +1,53 @@
+
+#include "../stdafx.h"
+#include <memory>
+#include <fstream>
+#include "FileSystemRawLoader.h"
+#include "../util/Overload.hpp"
+
+namespace windows {
+	using namespace Engine::Resources;
+
+	FileSystemLoader::FileSystemLoader(std::filesystem::path root_path) : mRootPath{ root_path } {}
+	FileSystemLoader::~FileSystemLoader() {
+
+	}
+
+	std::optional<RawResource> FileSystemLoader::load_file(const std::filesystem::path & rel_path) {
+
+	}
+
+	std::optional<std::filesystem::path> extract_id(RawID id) {
+		return Util::match(id, [](const RawID_IntID & id) { return {}; },
+			[](const std::filesystem::path & path) { return path; }
+		);
+	}
+
+	std::optional<RawResource> FileSystemLoader::read_item(RawID id) const {
+		if (auto fid = extract_id(id); fid)
+		{
+			auto file_path = mRootPath / fid.value();
+			if (std::filesystem::exists(file_path)) {
+				auto mem_size = std::filesystem::file_size(file_path);
+				
+				RawResource outpt{ std::make_unique<std::byte>(mem_size),
+					static_cast<std::size_t>(mem_size),
+					{fid.value} 
+				};
+
+				std::ifstream reader{ file_path };
+				reader.read((char*)outpt.file_data.get(), outpt.size);
+				return std::optional<RawResource>{std::move(outpt)};
+			}
+		}
+		return {};
+	}
+
+	bool FileSystemLoader::has_item(RawID id) const {
+		if (auto fid = extract_id(id); fid) {
+			auto file_path = mRootPath / fid.value();
+			return std::filesystem::exists(file_path);
+		}
+		return false;
+	}
+}
